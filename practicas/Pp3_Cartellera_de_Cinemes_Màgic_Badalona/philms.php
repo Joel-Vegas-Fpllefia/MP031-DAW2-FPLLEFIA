f4de2417af74d21a44a08036d6960d9fe91908c7
Joel-Vegas-Fpllefia <vegasromerojoel@fpllefia.com>
2025-09-26 16:30:12 +0000

    Agregar información detallada de películas a la cartelera, incluyendo nombre, imagen, horarios, sinopsis, duración, director, reparto, calificación y género.

---
+++ practicas/Pp3_Cartellera_de_Cinemes_Màgic_Badalona/philms.php
@@ -0,0 +1,213 @@
+<?php
+    $peliculas = [
+        [
+            "nom" => "Inception",
+            "imatge" => "https://m.media-amazon.com/images/I/912AErFSBHL._UF1000,1000_QL80_.jpg",
+            "horaris" => ["16:00", "19:00", "22:00"],
+            "sinopsi" => "Un lladre que roba secrets a través dels somnis és contractat per implantar una idea en la ment d'una persona.",
+            "durada" => "148 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+            "qualificacio" => "+16",
+            "genere" => "Ciència-ficció, Thriller",
+            "trailer" => "https://www.youtube.com/watch?v=YoHD9XEInc0"
+        ],
+        [
+            "nom" => "The Dark Knight",
+            "imatge" => "https://m.media-amazon.com/images/I/91WJMJ56GTL.jpg",
+            "horaris" => ["15:30", "18:30", "21:30"],
+            "sinopsi" => "Batman s'enfronta al Joker, un criminal que vol sembrar el caos a Gotham City.",
+            "durada" => "152 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Crim, Drama",
+            "trailer" => "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+        ],
+        [
+            "nom" => "Parasite",
+            "imatge" => "https://m.media-amazon.com/images/I/71c05lTE03L._AC_SY679_.jpg",
+            "horaris" => ["17:00", "20:00"],
+            "sinopsi" => "Una família pobre s'infiltra en una família rica amb conseqüències inesperades.",
+            "durada" => "132 min",
+            "director" => "Bong Joon-ho",
+            "repartiment" => ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+            "qualificacio" => "+16",
+            "genere" => "Drama, Thriller",
+            "trailer" => "https://www.youtube.com/watch?v=5xH0HfJHsaY"
+        ],
+        [
+            "nom" => "Interstellar",
+            "imatge" => "https://es.web.img3.acsta.net/pictures/14/10/02/11/07/341344.jpg",
+            "horaris" => ["14:00", "17:30", "21:00"],
+            "sinopsi" => "Un grup d'exploradors utilitza un forat de cuc per salvar la humanitat.",
+            "durada" => "169 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+            "qualificacio" => "+16",
+            "genere" => "Ciència-ficció, Drama",
+            "trailer" => "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+        ],
+        [
+            "nom" => "Joker",
+            "imatge" => "https://es.web.img3.acsta.net/pictures/19/09/17/17/03/5442885.jpg",
+            "horaris" => ["16:30", "19:30", "22:30"],
+            "sinopsi" => "Un home amb problemes mentals es converteix en el famós Joker de Gotham.",
+            "durada" => "122 min",
+            "director" => "Todd Phillips",
+            "repartiment" => ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
+            "qualificacio" => "+16",
+            "genere" => "Drama, Crim, Thriller",
+            "trailer" => "https://www.youtube.com/watch?v=zAGVQLHvwOY"
+        ],
+        [
+            "nom" => "Avengers: Endgame",
+            "imatge" => "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg",
+            "horaris" => ["15:00", "18:00", "21:00"],
+            "sinopsi" => "Els Avengers es reuneixen per desfer els efectes de Thanos i salvar l'univers.",
+            "durada" => "181 min",
+            "director" => "Anthony Russo, Joe Russo",
+            "repartiment" => ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Aventura, Ciència-ficció",
+            "trailer" => "https://www.youtube.com/watch?v=TcMBFSGVi1c"
+        ],
+        [
+            "nom" => "The Matrix",
+            "imatge" => "https://m.media-amazon.com/images/I/51EG732BV3L._AC_SY679_.jpg",
+            "horaris" => ["14:30", "17:30", "20:30"],
+            "sinopsi" => "Un hacker descobreix la realitat oculta darrere del món en què viu.",
+            "durada" => "136 min",
+            "director" => "Lana Wachowski, Lilly Wachowski",
+            "repartiment" => ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Ciència-ficció",
+            "trailer" => "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+        ],
+        [
+            "nom" => "La La Land",
+            "imatge" => "https://es.web.img3.acsta.net/pictures/16/11/30/17/44/581119.jpg",
+            "horaris" => ["15:00", "18:00", "21:00"],
+            "sinopsi" => "Una història d'amor entre una aspirant a actriu i un pianista de jazz a Los Angeles.",
+            "durada" => "128 min",
+            "director" => "Damien Chazelle",
+            "repartiment" => ["Ryan Gosling", "Emma Stone"],
+            "qualificacio" => "+16",
+            "genere" => "Comèdia, Drama, Musical",
+            "trailer" => "https://www.youtube.com/watch?v=0pdqf4P9MB8"
+        ],
+        [
+            "nom" => "Gladiator",
+            "imatge" => "https://m.media-amazon.com/images/M/MV5BYWJjYWJhZmMtOGM1My00YWQzLWFlNWEtZTQ3ZjM5NzAxZDNkXkEyXkFqcGc@._V1_.jpg",
+            "horaris" => ["16:00", "19:00", "22:00"],
+            "sinopsi" => "Un general romà traït busca venjança com a gladiador.",
+            "durada" => "155 min",
+            "director" => "Ridley Scott",
+            "repartiment" => ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Drama, Aventura",
+            "trailer" => "https://www.youtube.com/watch?v=owK1qxDselE"
+        ],
+        [
+            "nom" => "Titanic",
+            "imatge" => "https://es.web.img3.acsta.net/medias/nmedia/18/86/91/41/19870073.jpg",
+            "horaris" => ["15:00", "18:30", "22:00"],
+            "sinopsi" => "Història d'amor a bord del famós vaixell Titanic que naufraga.",
+            "durada" => "195 min",
+            "director" => "James Cameron",
+            "repartiment" => ["Leonardo DiCaprio", "Kate Winslet"],
+            "qualificacio" => "+16",
+            "genere" => "Drama, Romance",
+            "trailer" => "https://www.youtube.com/watch?v=2e-eXJ6HgkQ"
+        ]
+    ];
+
+    function creat_div(){
+        global $peliculas;
+        foreach($peliculas as $pelicula){
+            echo "<div class='pelicula'> <h1 class='ocult'> {$pelicula["nom"]} </h1>";
+            echo "<img src='{$pelicula['imatge']}' class='visible'  alt='{$pelicula["nom"]}'>";
+            echo "<p class='ocult'> Director: {$pelicula["director"]} </p>";
+            echo "<ul class='ocult'>";
+            show_timer($pelicula["horaris"]);
+            echo "</ul>";
+            echo "<p class='ocult'> Durada: {$pelicula["durada"]} </p>";
+            echo "<p class='ocult'> Descripcio: </br> {$pelicula['sinopsi']} </p>";
+            echo " </div>";
+        }
+    }
+
+    function show_timer($pelicula_list_time){
+        foreach ($pelicula_list_time as $time){
+            echo "<li> <p> [ $time ] </p> </li>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<style>
+    h1{
+        text-align: center;
+    }
+    hr{
+        margin-bottom: 10px;
+    }
+
+    #peliculas{
+        display: grid;
+        grid-template-columns: repeat(5, 1fr); 
+        gap: 10px; 
+        align-items: center;
+        justify-content: center;
+        margin-left: 100px;
+    }
+    .pelicula{
+        width: 300px;
+        height: 600px;
+        border: 1px solid black;
+        border-radius: 32px;
+        text-align: center;
+    }
+
+    .pelicula:hover .visible{
+        visibility: hidden;
+    }    
+    .pelicula:hover .ocult{
+        visibility: visible;
+    }
+    .ocult{
+        visibility: hidden;
+        position: relative;
+    }
+
+    ul{
+        list-style: none;
+        display: flex;
+    }
+    div >  h1{
+        font-size: 20px;
+    } 
+    div > img{
+        border-radius:16px;
+        width: 280px;
+        height: 300px;
+    }
+</style>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cartelera de No Cine</h1>
+    <hr>
+    <div id="peliculas">
+        <?php  creat_div() ?>
+    </div>
+    
+    
+</body>
+</html>
